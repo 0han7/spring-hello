8912e1f5a4c422c068a10886d7b9b8f2377c1eeb
Younghan Yonn <dudgks1172@naver.com>
2024-11-18 20:29:57 +0900

    회원 웹 기능 - 홈 화면 추가

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -13,4 +14,8 @@ public class HomeController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
