1077b3bc966bd6cb570188fe4b505354d3496ca5
Younghan Yonn <dudgks1172@naver.com>
2024-11-19 10:02:31 +0900

    회원 웹 기능 - 조회 기능

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -18,4 +20,20 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/member/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/member/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
